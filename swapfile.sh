e8bbcab075fff16955332e6c3526aacf4c109930
Ryan Price <ryapric@gmail.com>
2018-11-02 15:26:52 -0500

    Initial commit.

---
+++ swapfile.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# EC2 micro instances; 1G is too small for packages like readr/haven compilation)
+# If yes, prompt to make a local swap file.
+
+sysmem0="$(cat /proc/meminfo | grep MemTotal | awk '{ print $2 }')"
+sysmem="$((sysmem / 1000))"
+if [ $sysmem -lt 1000 ]; then
+    sysmem="'0.'$((sysmem0 / 1000))"
+fi
+
+echo "Available memory: ${sysmem} KB"
+echo "Would you like to create a swapfile (~/{x}g.swap)? (y/n) "
+read make_swap
+if [ "$make_swap" == "y" ]; then
+    echo "How large of a swapfile would you like?"
+    echo "Please enter as an integer, in GB (at least 1) : "
+    read swap_size
+    swapfile="~/${swap_size}g.swap"
+    swapoff -a
+    fallocate -l ${swap_size}g "$swapfile"
+    chmod 600 "$swapfile"
+    mkswap "$swapfile"
+    swapon "$swapfile"
+fi
